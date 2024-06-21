6c0a600af0b2dc39d6843037e51c8986c5b6f8ec
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2024-06-21 14:47:19 -0300

    animations & parallax

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+const scrollElements = document.querySelectorAll(".js_scroll");
+const planet = document.querySelector(".hero_planet");
+const rocks = document.querySelector(".hero_rocks");
+const bg = document.querySelector(".hero_bg");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  const value = window.scrollY;
+  handleScrollAnimation();
+  planet.style.left = value * 0.2 + "px";
+  rocks.style.top = value * 0.3 + "px";
+  bg.style.top = value * 0.2 + "px";
+});
